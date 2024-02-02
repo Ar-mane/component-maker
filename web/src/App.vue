af7e499a0dfc94363d4324d894f3ef989195fd7b
Ar-mane <abderrahmane.arache@gmail.com>
2024-02-02 19:32:31 +0100

    feat: update  web's readme

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import CmHello from './components/CmHello.vue';
 import { marked } from 'marked'
 
 const readmeContent = ref<string>(''); // to store the content of the README
@@ -22,8 +21,11 @@ onMounted(async () => {
 <template>
   <div class="w-full flex items-center text-center flex-col p-20">
     <a href="https://marketplace.visualstudio.com/items?itemName=Ar-mane.component-maker" target="_blank">
-      <div> <img src="/ico.svg" class="w-60" alt="CM Logo" /></div>
-      <CmHello class="bg-blue-100 rounded-lg p-5 hover:bg-blue-400" msg="Component Maker" />
+      <img src="/ico.svg" class="w-60" alt="CM Logo" />
+    </a>
+    <a href="https://github.com/Ar-mane/component-maker" target="_blank">
+      <div class="flex flex-row gap-2 bg-blue-100 rounded-lg p-5 hover:bg-blue-400"> <img src="/github-logo.png"
+          class="w-6" alt="contibute" /> Contibute ? </div>
     </a>
     <div v-html="marked(readmeContent)"></div>
   </div>
